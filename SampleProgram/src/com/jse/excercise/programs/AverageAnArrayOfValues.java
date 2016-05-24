9f993c101dac7b69c1500993baebf89e4aec13ae
Ramya Simhadri <ramyasimhadri22@gmail.com>
2016-05-24 12:20:23 -0400

    P29:Program to get average an array of values

---
+++ SampleProgram/src/com/jse/excercise/programs/AverageAnArrayOfValues.java
@@ -0,0 +1,16 @@
+package com.jse.excercise.programs;
+
+public class AverageAnArrayOfValues {
+
+	public static void main(String args[]) {
+
+		double nums[] = { 10.1, 11.2, 12.3, 13.4, 14.5 };// Here We Define Array
+		double result = 0; // Result variable to Store the sum of array of
+							// values
+		int i;
+
+		for (i = 0; i < 5; i++)
+			result = result + nums[i]; // addition of Values
+		System.out.println("Average is " + result / 5);
+	}
+}
